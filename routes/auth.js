6d5c5962aee410c58375660bc9ef840f4e5538e4
PaulB-H <p.bernardhall@gmail.com>
2021-01-30 22:52:48 -0500

    Fixed route filepath in server.js, fixed router const in all 3 route files

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 
 // @route   GET api/auth
 // @desc    Get logged in user
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 
 // @route   POST api/users
 // @desc    Register a user
