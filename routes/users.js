d435621d9261ae8e0c01ec91783390ee9c891bca
PaulB-H <p.bernardhall@gmail.com>
2021-02-01 18:20:16 -0500

    Set users.js endpoint function to be async

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
