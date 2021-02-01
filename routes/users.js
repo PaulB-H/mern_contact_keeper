ce7a200e5682c923829e73c38bc784a47afa2346
PaulB-H <p.bernardhall@gmail.com>
2021-02-01 17:28:17 -0500

    Imported express-validation to users.js & added checks for user fields

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post("/", (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add name").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
