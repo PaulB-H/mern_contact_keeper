ca860a5325d3b8f44c782048d342433b0122d1b7
PaulB-H <p.bernardhall@gmail.com>
2021-02-01 17:07:07 -0500

    Refactored DB connection to use async/await & try/catch

--- config/db.js
+++ config/db.js
@@ -3,18 +3,19 @@ const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
-  mongoose
-    .connect(db, {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => {
-      console.error(err.message);
-      process.exit(1);
     });
+
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
