890a40c21a85e411afc539e8333755756018b1f1
PaulB-H <p.bernardhall@gmail.com>
2021-02-05 16:39:57 -0500

    App.js: Wrapped app in router, replaced div with fragment, added Switch inside container div

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,14 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      My App
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div class="container">
+          <Switch></Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
