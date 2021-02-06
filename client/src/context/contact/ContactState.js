8a72771248fb7379f2387c071fbb7920cdab3c10
PaulB-H <p.bernardhall@gmail.com>
2021-02-06 16:29:18 -0500

    ContactState.js: Setup structure for our actions w/comments

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -40,4 +40,18 @@ const ContactState = (props) => {
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+
+  // Filter Contacts
+
+  // Clear Filter
 };
