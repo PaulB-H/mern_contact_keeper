86f1e5dfdff26f52937568e881782a365a58e609
PaulB-H <p.bernardhall@gmail.com>
2021-02-06 16:17:29 -0500

    ContactState.js: Setup initial state using placeholder data

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -11,3 +11,31 @@ import {
   FILTER_CONTACTS,
   CLEAR_FILTER,
 } from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Jack Johnson",
+        email: "jjohn@gmail.com",
+        phone: "111-111-1111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Jill Johnson",
+        email: "jill@gmail.com",
+        phone: "222-222-2222",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "333-333-3333",
+        type: "professional",
+      },
+    ],
+  };
+};
