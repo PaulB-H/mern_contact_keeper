2b6a904e03abfc2e68e8e2aaf74ef33b224e10b5
PaulB-H <p.bernardhall@gmail.com>
2021-02-07 16:12:39 -0500

    Contacts.js: Added imports & created arrow component

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,8 @@
+import React, { useContext, Fragment } from "react";
+import ContactContext from "../../context/contact/contactContext";
+
+const Contacts = () => {
+  return <div></div>;
+};
+
+export default Contacts;
