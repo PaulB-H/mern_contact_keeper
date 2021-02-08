ede176fa616ab8abf1eb417deb9a7d64dfdb26cb
PaulB-H <p.bernardhall@gmail.com>
2021-02-08 15:13:31 -0500

    ContactForm.js: Setup component level state for form use
    Also destructured values from contact

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 
 const ContactForm = () => {
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    type: "personal",
+  });
+
+  const { name, email, phone, type } = contact;
+
   return <div></div>;
 };
 
