14a88621c8bfed1800c3ffaec74642e5bc309a8d
PaulB-H <p.bernardhall@gmail.com>
2021-02-08 22:20:30 -0500

    ContactForm.js: Setup form inputs

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -10,7 +10,54 @@ const ContactForm = () => {
 
   const { name, email, phone, type } = contact;
 
-  return <div></div>;
+  return (
+    <form>
+      <h2 class="text-primary">Add Contact</h2>
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type="phone"
+        placeholder="Phone"
+        name="phone"
+        value={phone}
+        onChange={onChange}
+      />
+      <h5>Contact type</h5>
+      <input
+        type="radio"
+        name="type"
+        value="personal"
+        checked={type === "personal"}
+      />{" "}
+      Personal{" "}
+      <input
+        type="radio"
+        name="type"
+        value="professional"
+        checked={type === "professional"}
+      />{" "}
+      Professional{" "}
+      <div>
+        <input
+          type="submit"
+          value="Add Contact"
+          className="btn btn-primary btn-block"
+        />
+      </div>
+    </form>
+  );
 };
 
 export default ContactForm;
