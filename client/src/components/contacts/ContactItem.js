4d0bf3ad6bcabc8443f260759a1ca231adb1c03c
PaulB-H <p.bernardhall@gmail.com>
2021-02-09 15:55:42 -0500

    ContactItem.js: Added useContact, ContactContext imports

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import ContactContext from "../../context/contact/contactContext";
 
 const ContactItem = ({ contact }) => {
   const { id, name, email, phone, type } = contact;
