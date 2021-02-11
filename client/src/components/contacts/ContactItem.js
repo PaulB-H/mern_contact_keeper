8a04c682358424bf2c693b2abd84b3786d90cfd8
PaulB-H <p.bernardhall@gmail.com>
2021-02-10 21:33:08 -0500

    ContactItem.js: Added clearCurrent to destructured values from context

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -4,7 +4,7 @@ import ContactContext from "../../context/contact/contactContext";
 
 const ContactItem = ({ contact }) => {
   const contactContext = useContext(ContactContext);
-  const { deleteContact, setCurrent } = contactContext;
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
   const { id, name, email, phone, type } = contact;
 
