88ff08bf70606e98860aa4eec840841d13ce8b7d
PaulB-H <p.bernardhall@gmail.com>
2021-02-16 16:37:27 -0500

    ContactState.js: Added filtered state to provider

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -103,6 +103,7 @@ const ContactState = (props) => {
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
