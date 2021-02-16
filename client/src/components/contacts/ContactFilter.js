2e94d664c004365b1a9d69fb8d3f2f86509ac47d
PaulB-H <p.bernardhall@gmail.com>
2021-02-16 18:02:12 -0500

    ContactFilter.js: Initialized text with useRef hook

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -3,6 +3,7 @@ import ContactContext from "../../context/contact/contactContext";
 
 const ContactFilter = () => {
   const contactContext = useContext(contactContext);
+  const text = useRef("");
 
   return <div></div>;
 };
