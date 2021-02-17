035f2a53f1e6d42675b0b1772534c4f98231ca2f
PaulB-H <p.bernardhall@gmail.com>
2021-02-17 07:00:53 -0500

    ContactFilter.js: Fixed improper capitalization in context initialization

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -2,7 +2,7 @@ import React, { useContext, useRef } from "react";
 import ContactContext from "../../context/contact/contactContext";
 
 const ContactFilter = () => {
-  const contactContext = useContext(contactContext);
+  const contactContext = useContext(ContactContext);
   const text = useRef("");
 
   return <div></div>;
