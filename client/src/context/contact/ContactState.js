67c75d9d1c18b3923900eba91e8baa935a6456f6
PaulB-H <p.bernardhall@gmail.com>
2021-02-17 07:33:02 -0500

    ContactState.js: Fixed incorrect capitalization for clearFilter

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -92,7 +92,7 @@ const ContactState = (props) => {
   };
 
   // Clear Filter
-  const clearfilter = () => {
+  const clearFilter = () => {
     dispatch({
       type: CLEAR_FILTER,
     });
@@ -110,7 +110,7 @@ const ContactState = (props) => {
         clearCurrent,
         updateContact,
         filterContacts,
-        clearfilter,
+        clearFilter,
       }}
     >
       {props.children}
