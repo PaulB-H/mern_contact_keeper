c015a91baeecfd6bf5699c8867cea866b772afd6
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-11-11 14:07:26 -0500

    Navbar: split into authLinks & guestLinks

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,25 +12,35 @@ const Navbar = ({ title, icon }) => {
     logout();
   };
 
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <i class="fas fa-sign-out-alt"></i>{" "}
+          <span class="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar bg-primary">
       <h1>
         <i className={icon} /> {title}
       </h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
   );
 };
